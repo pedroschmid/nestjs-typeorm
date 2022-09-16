644a85f82b2e1410f1e84f0d9b82d7aa66d091e2
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-09-16 01:57:45 -0300

    Created app module containing all modules

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { PostgresModule } from 'src/database/postgres.module';
+import { MailModule } from 'src/mail/mail.module';
+import { HealthModule } from 'src/health/health.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
+import { OrderModule } from 'src/order/order.module';
+import { AddressModule } from 'src/address/address.module';
+import { CardModule } from 'src/card/card.module';
+
+@Module({
+  imports: [
+    /* Environment variables */
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    /* Database module */
+    PostgresModule,
+    /* Mailer module */
+    MailModule,
+    /* Health module */
+    HealthModule,
+    /* Application modules */
+    AuthModule,
+    UserModule,
+    OrderModule,
+    AddressModule,
+    CardModule,
+  ],
+})
+export class AppModule {}
